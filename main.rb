d11d8073d5614ef33f63426e402c890b42d823aa
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-06 19:01:58 -0700

    Rename back to draw_button.

--- main.rb
+++ main.rb
@@ -2,6 +2,6 @@ def draw_button_dark_mode(label_text, x, y, foreground_color, color_code)
   paint(label_text, x, y, foreground_color - 10, '#111111')
 end
 
-def paint(label_text, x, y, foreground_color, color_code)
+def draw_button(label_text, x, y, foreground_color, color_code)
   paint(label_text, x, y, foreground_color + 10, '#111111')
 end
