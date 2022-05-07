61cb71b82de7bc2d8cf2d60e57dcca6912fcc9eb
Jonah Broyer <jkbsk8er@yahoo.com>
2022-05-06 18:52:53 -0700

    Refactored long parameter list by removing is_dark_mode flag argument.

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  if is_dark_mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-  else
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-  end
+def draw_button_dark_mode(label_text, x, y, foreground_color, color_code)
+  paint(label_text, x, y, foreground_color - 10, '#111111')
+end
+
+def paint(label_text, x, y, foreground_color, color_code)
+  paint(label_text, x, y, foreground_color + 10, '#111111')
 end
